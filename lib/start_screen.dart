206e9940ec170b2e26594c7e792c0a669866c607
Bishwo Mainali <bishworajmainali1@gmail.com>
2024-11-04 01:03:39 -0500

    Created Start Screen and Blank stateful widget

---
+++ lib/start_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget{
+  const StartScreen({super.key});
+  @override
+  Widget build(BuildContext context){
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min ,
+        children: [
+          Opacity( 
+            opacity: .75,
+
+            child: Image.asset(
+              'assets/images/quiz-logo.png',
+               width:300
+          ),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            "Try your luck on our quiz!!!!",
+            style: TextStyle(
+              color:  Colors.white,
+              fontSize:24,
+              ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            icon: const Icon(
+              Icons.arrow_circle_right_outlined,
+              size:40
+              ),
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+            onPressed: () {},
+            label: const Text(
+              "Start Quiz",
+              style: TextStyle(
+                fontSize: 24
+              )
+              ),
+          )
+          
+        ],
+        ),
+    );
+  }
+}
